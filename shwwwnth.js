ba870e23804f4c68c98e337ec54d5c57035280b9
Bryce Culhane <bryce.culhane@gmail.com>
2014-02-18 14:31:55 -0600

    Update shwwwnth.js

--- shwwwnth.js
+++ shwwwnth.js
@@ -110,7 +110,7 @@ run = function(array) {
             } else if (Array.isArray(array[i])) {
                 other = run(array[i]);
                 console.log(op, other);
-                other.connect(op, 0, i-1);
+                other.connect(op, 0, i-0.5);
             } else {
                 console.log('something weird: ', array[i]);
             }
